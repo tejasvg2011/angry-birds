14cc58c3dcf9a9fd4d0820125c6c06ba37c62124
Tejas V G <tejasvg65@gmail.com>
2021-02-11 20:51:47 +0530

    added Bird

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var bird2;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +36,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    bird2= new Bird(100,150);
 
 }
 
@@ -60,5 +62,6 @@ function draw(){
     log5.display();
 
     bird.display();
+    bird2.display();
     platform.display();
 }
